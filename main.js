e76542c97e8163eda9d84b674a939f8623576bcc
jessdcodes <dionisiojessica15@gmail.com>
2022-09-28 21:51:03 +0800

    Add grid size input (range) and display

--- main.js
+++ main.js
@@ -127,8 +127,22 @@ function reduceRGB(rgb){
     return rgbStr;
 }
 
+function updateGridSizeDisplay(){
+
+}
+
+function addSizeRangeActions(){
+    let sizeSlider = document.querySelector("#size-slider");
+    sizeSlider.addEventListener("change", (e) => {
+        console.log(e.value);
+        updateGridSizeDisplay(e.value);
+        loadBoard(e.value);
+    });
+}
+
 loadBoard(16);
 
 addBtnActions();
 addModeToggleActions();
 enableColorPicker();
+addSizeRangeActions();
